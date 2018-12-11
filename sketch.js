d2b8a215788bfa70884b2a71e4e5a50859c3e82c
melisaaltinsoy <32497953+melisaaltinsoy@users.noreply.github.com>
2018-12-12 00:45:30 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,53 +1,59 @@
 var inc = 0.01;
-var scl = 10; //scale
-var cols, rows;
+var scl = 5; //scale
+var cols,rows;
 
 var zoff=0;
 
 var particles = [];
 
+var flowfield;
+
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(1600, 900);
+  cols = floor(width/scl);
+  rows = floor(height/scl);
   
-  //pixelDensity(1); //we did for mac retina WHEN WE USE PIXEL FUNCTION
-  cols = floor((width/2)/scl);
-  rows = floor((height/2)/scl);
+  flowfield = new Array(cols * rows);
   
-  for(var i=0; i<100;i++){
-   particles[i] = new Particle();
+  for (var i=0; i<10000; i++){
+     particles[i] = new Particle();
   }
+   background(0);
 }
 
 function draw() {
-  background(0);
-  
-  var yoff = 0;
-  
  
-  for(var x = 0; x<rows; x++) {
+  var yoff = 0;
+  for(var y = 0; y<rows; y++) {
       var xoff = 0;
-      for(var y = 0; y<cols; y++) {
-        var index = (x + y*width)*4;
-        var angle = noise(xoff,yoff,zoff)*TWO_PI;
-        //var v = createVector();
+      for(var x = 0; x<cols; x++) {
+        var index = x + y * cols;
+        var angle = noise(xoff,yoff,zoff)*TWO_PI*4;
         var v = p5.Vector.fromAngle(angle);
+        v.setMag(1);
+        flowfield[index] = v;
         xoff += inc;
-        stroke(255);
-        push();
-        translate(x*scl,y*scl);
-        rotate(v.heading());
-        line(0,0,scl,0);
-        
-        pop();
+        stroke(255,50);
+        //push();
+        //translate(x*scl,y*scl);
+        //rotate(v.heading());
+        //strokeWeight(1);
+        //line(0,0,scl,0);       
+        //pop();
       }
-    yoff += inc;
     
-    zoff += 0.01;
+     yoff += inc;
+    
+     zoff += 0.001;
   }  
   
-  for(var i=0; i<particles.length;i++){
-    particles[i].update();
-    particles[i].show();
+  for (var i=0; i< particles.length; i++){
+     particles[i].follow(flowfield);
+     particles[i].update();
+     particles[i].edges();
+     particles[i].show();
+     
   }
   
 }
